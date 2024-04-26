dfda0cff65f3c29304bb8d47fb65d3789b996da8
chophandog <chophandog@gmail.com>
2024-04-26 10:56:42 +0700

    update fea

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ function App() {
 		totalWin: 0,
 		win: undefined,
 		ratioWin: 0,
+		radio: 0,
 	});
 
 	useEffect(() => {
-		result(state.rateMin, state.rateMax, state.betMin, state.betMax);
-	}, [state.rateMin, state.rateMax, state.betMin]);
+		result(state.rateMin, state.rateMax, state.betMin, state.radio);
+	}, [state.rateMin, state.rateMax, state.betMin, state.radio]);
 
 	const onRateMinChange = (e) => {
 		setState((prevState) => ({ ...prevState, rateMin: e.target.value }));
@@ -26,23 +27,56 @@ function App() {
 	const onBetMinChange = (e) => {
 		setState((prevState) => ({ ...prevState, betMin: +e.target.value }));
 	};
+	const onRadioChange = (e) => {
+		setState((prevState) => ({ ...prevState, radio: +e.target.value }));
+	};
 
-	const result = (rateMin, rateMax, betMin, betMax) => {
+	const result = (rateMin, rateMax, betMin, radio) => {
 		if (rateMin && rateMin !== 0 && rateMax && rateMax !== 0 && betMin && betMin !== 0) {
 			//1.8 || 2.4
-			const ratio = rateMax / rateMin;
-			const totalBet = Math.round(betMin + ratio * betMin);
-			const totalWin = Math.round(betMin * rateMax);
-			const win = totalWin - totalBet;
-			const ratioWin = ((win / totalBet) * 100).toFixed(1);
-			setState((prevState) => ({
-				...prevState,
-				betMax: Math.round(ratio * betMin),
-				totalBet: totalBet,
-				totalWin: totalWin,
-				win: win,
-				ratioWin: ratioWin,
-			}));
+			if (radio === 0) {
+				const ratio = rateMax / rateMin;
+				const totalBet = Math.round(betMin + ratio * betMin);
+				const totalWin = Math.round(betMin * rateMax);
+				const win = totalWin - totalBet;
+				const ratioWin = ((win / totalBet) * 100).toFixed(1);
+				setState((prevState) => ({
+					...prevState,
+					betMax: Math.round(ratio * betMin),
+					totalBet: totalBet,
+					totalWin: totalWin,
+					win: win,
+					ratioWin: ratioWin,
+				}));
+			} else if (radio === 1) {
+				const betMax = betMin * rateMax - betMin;
+				const totalBet = Math.round(betMin + betMax);
+				const totalWin = Math.round(betMax * rateMin);
+				const win = totalWin - totalBet;
+				const ratioWin = ((win / totalBet) * 100).toFixed(1);
+				setState((prevState) => ({
+					...prevState,
+					betMax: Math.round(betMax),
+					totalBet: totalBet,
+					totalWin: totalWin,
+					win: win,
+					ratioWin: ratioWin,
+				}));
+			} else if (radio === 2) {
+				const betMax = betMin / (rateMin - 1);
+				const totalBet = Math.round(betMin + betMax);
+				const totalWin = Math.round(betMin * rateMax);
+				const win = totalWin - totalBet;
+				const ratioWin = ((win / totalBet) * 100).toFixed(1);
+				setState((prevState) => ({
+					...prevState,
+					betMax: Math.round(betMax),
+					totalBet: totalBet,
+					totalWin: totalWin,
+					win: win,
+					ratioWin: ratioWin,
+				}));
+			}
 		}
 	};
 
@@ -58,12 +92,59 @@ function App() {
 				<p>Lộ</p>
 			) : (
 				<>
+					<div style={{ display: "flex", alignItems: "center", justifyContent: "space-around", width: "100%" }}>
+						<div style={{ display: "flex", gap: "8px" }}>
+							<input
+								style={{ width: "unset", cursor: "pointer" }}
+								type="radio"
+								id={0}
+								value={0}
+								checked={state.radio === 0}
+								onChange={onRadioChange}
+								name="line"
+							/>
+							<label htmlFor={0}>Chia 2</label>
+						</div>
+						<div style={{ display: "flex", gap: "8px" }}>
+							<input
+								style={{ width: "unset", cursor: "pointer" }}
+								type="radio"
+								id={1}
+								value={1}
+								checked={state.radio === 1}
+								onChange={onRadioChange}
+								name="line"
+							/>
+							<label htmlFor={1}>rate nhỏ ăn</label>
+						</div>
+						<div style={{ display: "flex", gap: "8px" }}>
+							<input
+								style={{ width: "unset", cursor: "pointer" }}
+								type="radio"
+								id={2}
+								value={2}
+								checked={state.radio === 2}
+								onChange={onRadioChange}
+								name="line"
+							/>
+							<label htmlFor={2}>rate bự ăn</label>
+						</div>
+					</div>
 					<p>bẹt rate to: {state.betMin}</p>
 					<p>bẹt rate nhỏ: {state.betMax}</p>
 					<p>Tổng bẹt: {state.totalBet}</p>
 					<p>Tổng win: {state.totalWin}</p>
 					<p>Tổng lời: {state.win}</p>
 					<p>Phần trăm lời: {state.ratioWin}%</p>
+					{state.radio !== 0 && (
+						<>
+							<br />
+							<p>Nếu kèo ngược lại ăn thì:</p>
+							<p>Tổng win: 0</p>
+							<p>Tổng lời: 0</p>
+							<p>Phần trăm lời: 0%</p>
+						</>
+					)}
 				</>
 			)}
 		</div>
